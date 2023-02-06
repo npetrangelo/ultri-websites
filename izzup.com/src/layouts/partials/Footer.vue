5f1e0ea625bd5fcc355cf86e04727fdaf02bbabf
Brian Winkers <bwinkers@gmail.com>
2023-02-05 17:42:49 -0800

    Fixed copyright year

--- izzup.com/src/layouts/partials/Footer.vue
+++ izzup.com/src/layouts/partials/Footer.vue
@@ -3,7 +3,7 @@
     <footer id="footer" class="footer bg-gray-200" role="contentinfo">
       <div class="footer-inner container m-auto md:flex justify-between items-center px-6 py-10">
         <p class="text-purple-900 pb-8 md:pb-0">
-          © 2019 Izzup.com
+          © 2023 Izzup.com
         </p>
 
         <ul class="flex justify-center">
