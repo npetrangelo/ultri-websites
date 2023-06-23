77048fca756f51c4e0617dcc447c28d28ed6365a
Brian Winkers <bwinkers@gmail.com>
2023-06-22 23:11:11 -0700

    Fixed typo

--- new-ultri/src/i18n/en-US/homepage-drawer.js
+++ new-ultri/src/i18n/en-US/homepage-drawer.js
@@ -2,7 +2,7 @@ export default {
   'homepage-drawer.join-the-discussion': 'Join the Discussion',
   'homepage-drawer.co-op-ownership.label': 'Co-op Ownership',
   'homepage-drawer.co-op-ownership.caption': 'Better together',
-  'homepage-drawer.co-op-ownership.blurb': 'By combing users and develoeprs as co-owners we eliminate the VC profiteering and the bloated layers of corporate management that represent the majority of the cost for most SaaS. End users and developers working together as equals, using sociocratic practices, will forge better products at better prices.',
+  'homepage-drawer.co-op-ownership.blurb': 'By combing users and developers as co-owners we eliminate the VC profiteering and the bloated layers of corporate management that represent the majority of the cost for most SaaS. End users and developers working together as equals, using sociocratic practices, will forge better products at better prices.',
   'homepage-drawer.user-ownership.label': 'Ownership for Users',
   'homepage-drawer.user-ownership.caption': 'no more surprises',
   'homepage-drawer.user-ownership.blurb': 'You own the software, so there will be no surprise feature or price changes. You know you are getting the best price available because excess profits are returned to you as an owner.',
