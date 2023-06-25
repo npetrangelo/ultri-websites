1b7c4f7f6e10dfb9967fe72a10aa3bedc33ac4cf
Brian Winkers <bwinkers@gmail.com>
2023-06-24 21:28:44 -0700

    Hmmm

--- ultri.com/docs/.vitepress/config.ts
+++ ultri.com/docs/.vitepress/config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     //    ariaLabel: 'Project Menu',
     //    items: [
     //      { text: 'Sociocracy Handbook', link: 'https://handbook.opensociocracy.org/' },
-    //      { text: 'Sociocracy Logbook', link: 'https://www.ultri.com/logbook/' }
+    //      { text: 'Sociocracy Logbook', link: 'https://www.ultri.com/logbook' }
     //    ]
     //  }
     ],
