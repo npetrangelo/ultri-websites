e75658d5c53b65c0a67b620aeedc31e04ee8842f
Brian Winkers <bwinkers@gmail.com>
2023-07-15 17:28:02 -0700

    Igtnore deadlinks for now

--- platform.ultri.com/docs/.vitepress/config.ts
+++ platform.ultri.com/docs/.vitepress/config.ts
@@ -1,36 +1,40 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  ignoreDeadLinks: true,
   title: "Ultri",
   description: "Build it - Use it - Own it",
   head: [
     [
-      'script',
-      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-GKG2LWGGKE' }
+      "script",
+      {
+        async: "",
+        src: "https://www.googletagmanager.com/gtag/js?id=G-GKG2LWGGKE",
+      },
     ],
     [
-      'script',
+      "script",
       {},
-      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-GKG2LWGGKE');"
-    ]
+      "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-GKG2LWGGKE');",
+    ],
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Own It', link: 'https://coop.ultri.com' },
-      { text: 'Developers', link: '/developer/' },
-      
-    //  { text: 'Projects', link: '/tech-coop-projects/' },
-    //  {
-    //    text: 'Projects',
-    //    ariaLabel: 'Project Menu',
-    //    items: [
-    //      { text: 'Sociocracy Handbook', link: 'https://handbook.opensociocracy.org/' },
-    //      { text: 'Sociocracy Logbook', link: 'https://logbook.opensociocracy.org/' }
-    //    ]
-    //  }
+      { text: "Home", link: "/" },
+      { text: "Own It", link: "https://coop.ultri.com" },
+      { text: "Developers", link: "/developer/" },
+
+      //  { text: 'Projects', link: '/tech-coop-projects/' },
+      //  {
+      //    text: 'Projects',
+      //    ariaLabel: 'Project Menu',
+      //    items: [
+      //      { text: 'Sociocracy Handbook', link: 'https://handbook.opensociocracy.org/' },
+      //      { text: 'Sociocracy Logbook', link: 'https://logbook.opensociocracy.org/' }
+      //    ]
+      //  }
     ],
 
     sidebar: {
@@ -45,15 +49,16 @@ export default defineConfig({
             {
               text: "Infrastructure",
               link: "/developer/backend/",
-            }
-          ]
-        }     
-    ]
-  },
+            },
+          ],
+        },
+      ],
+    },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/Ultri-Izzup/ultri-websites' },
+      { icon: "github", link: "https://github.com/Ultri-Izzup/ultri-websites" },
       // icon: 'github', link: 'https://github.com/Ultri-Izzup/ultri-infrastructure' }
-    ]
-  }
-})
+    ],
+
+  },
+});
